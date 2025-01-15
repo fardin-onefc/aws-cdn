42aee9ae8d0d68ef5c8e8246cca4c62a34d32df1
Fardin Shaik <s.fardin@onefc.com>
2025-01-15 16:05:55 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const cors = require('@fastify/cors');
+
+const fastify = require('fastify')({
+	logger: true
+});
+
+fastify.register(cors);
+
+fastify.get('/', async (request, reply) => {
+	return { hello: 'world' };
+});
+
+fastify.get('/preprod-landing-page', async (request, reply) => {
+	console.log(JSON.stringify(request.headers, null, 2));
+	const response = await axios.get(
+		'https://oneapp.preprod.tech.onefc.com/content/api/v1/events?skip=0&limit=10&type=past',
+		{
+			headers: {
+				'Content-Type': 'application/json',
+				locale: 'en-us'
+			}
+		}
+	);
+	return reply.send(response.data);
+});
+
+const start = async () => {
+	try {
+		await fastify.listen({ port: 3000 });
+	} catch (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+};
+start();
